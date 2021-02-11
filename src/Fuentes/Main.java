d1dd3456684c8e120ca8c3cf33c93a2396c9c35a
Andrés Ponce <Usuario@192.168.200.16>
2021-02-11 09:31:01 -0500

    Implementación de la base de datos.

--- src/Fuentes/Main.java
+++ src/Fuentes/Main.java
@@ -13,7 +13,7 @@ public class Main {
    
     public static void main(String[] args) throws ClassNotFoundException, SQLException{
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String connectionURL= "jdbc:sqlserver://DESKTOP-KQSHIE7:1433;databaseName=AEREOPUERTOBD;user=sa;password=P@ssw0rd;";
+        String connectionURL= "jdbc:sqlserver://sql5054.site4now.net;databaseName=DB_A6F631_AEREOPUERTOBD;user=DB_A6F631_AEREOPUERTOBD_admin;password=P@ssw0rd;";
         Connection con = DriverManager.getConnection(connectionURL);
         System.out.println("Conexión exitosa");
         
