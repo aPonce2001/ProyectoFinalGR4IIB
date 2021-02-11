8c4bbe38c9c00f01a105286bc483b9c9ba9f910f
Andrés Ponce <Usuario@192.168.200.16>
2021-02-11 07:57:50 -0500

    Implementación de la base de datos.

---
+++ src/Fuentes/Main.java
@@ -0,0 +1,32 @@
+package Fuentes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//@author Kelvin Ojeda
+
+public class Main {
+
+   
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionURL= "jdbc:sqlserver://DESKTOP-KQSHIE7:1433;databaseName=AEREOPUERTOBD;user=sa;password=P@ssw0rd;";
+        Connection con = DriverManager.getConnection(connectionURL);
+        System.out.println("Conexión exitosa");
+        
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from RESERVA");
+        System.out.println("Tabla vuelos\n");
+        while(rs.next()){
+            int id_reserva = rs.getInt(1);
+            int pasaporte= rs.getInt(2);
+            String idVuelo=  rs.getString(3);
+            int asiento= rs.getInt(4);
+            System.out.println(id_reserva+ " "+pasaporte+" "+idVuelo+" "+asiento);
+        }
+    }
+    
+}
